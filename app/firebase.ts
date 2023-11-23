ee9ad79848aad28b69437e2ad8d57ca9ccedb0bf
Jonas Gabriel <jgalmeida1993@gmail.com>
2023-11-22 21:21:27 -0300

    refactor: add env variables

--- app/firebase.ts
+++ app/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyApK-ZP7ifaWcY6pRuTCRqpu5AufUqXEmI",
-  authDomain: "cnt-keys.firebaseapp.com",
-  projectId: "cnt-keys",
-  storageBucket: "cnt-keys.appspot.com",
-  messagingSenderId: "45587695935",
-  appId: "1:45587695935:web:5ee5e2d6d06aa2afbf6b8a",
-  measurementId: "G-WXV0E1PLCZ",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
